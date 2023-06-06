1f2f03db910cf4171a2ea68d8fa6fdb0f464cb1f
Sunil Kumar Das <sunilcet488@gmail.com>
2023-06-06 21:17:21 +0530

    First Commit

---
+++ RestServiceCloudBTPDB/src/main/java/com/abapskill/RestServiceCloudBtpdbApplication.java
@@ -0,0 +1,13 @@
+package com.abapskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceCloudBtpdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceCloudBtpdbApplication.class, args);
+	}
+
+}
